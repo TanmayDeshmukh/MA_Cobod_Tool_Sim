8ea692205a06d9a0130fc0e47fa2a61b84d5ed99
Tanmay <tanmay.dn@gmail.com>
2021-02-06 20:04:24 +0100

    point translation and matlab plotting

--- 20mm_cube_test.py
+++ 20mm_cube_test.py
@@ -6,6 +6,12 @@ import os
 import numpy as np
 import inspect
 import trimesh
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+
+
 
 from trimesh.exchange.binvox import voxelize_mesh
 from trimesh import voxel as v
@@ -14,11 +20,17 @@ mesh = trimesh.load_mesh('models/20mm_cube.stl')
 #print('faces_mask', faces_mask)
 #mesh.update_faces(faces_mask)
 
-y_extents = mesh.bounds[:,2]
+min_lim = min(mesh.bounds[0,:])
+max_lim = max(mesh.bounds[1,:])
+ax.set_xlim3d(min_lim, max_lim)
+ax.set_ylim3d(min_lim,max_lim)
+ax.set_zlim3d(min_lim,max_lim)
+
+y_extents = mesh.bounds[:, 2]
 # slice every .10 model units
 y_levels  = np.arange(*y_extents, step=10)#-mesh.bounds[0][0]
 # https://github.com/mikedh/trimesh/issues/743#issuecomment-642157661
-mesh.show()
+# mesh.show()
 # find a bunch of parallel cross sections
 print('mesh.bounds', mesh.bounds, 'y_extents', y_extents, 'y_levels', y_levels)
 sections = mesh.section_multiplane(plane_origin=[0,0,0],
@@ -38,10 +50,14 @@ print('face_normals', face_normals)
 
 
 standoff_dist = 1
+new_section_entities = []
+new_d3sections = []
+
 for section_iter, section_path_group in enumerate(d3sections):
     #path_group.show()
-    print('path_group',type(section_path_group) ,section_path_group, 'path_group attribs:\n', section_path_group.__dict__ , '\n')
+    print('path_group', type(section_path_group) ,section_path_group, 'path_group attribs:\n', section_path_group.__dict__ , '\n')
     face_indices = section_path_group.metadata['face_index']
+
 
     for subpath_iter, subpath in enumerate(section_path_group.entities):
         translated_verts = []
@@ -49,27 +65,42 @@ for section_iter, section_path_group in enumerate(d3sections):
         for line_segment_index in range(len(subpath.points)-1):
             this_face_normal = mesh.face_normals[face_indices[line_segment_index]]
             vert1_index = subpath.points[line_segment_index]
-
+            vert2_index = subpath.points[line_segment_index+1]
             vert1 = section_path_group.vertices[vert1_index]
-            vert2 = section_path_group.vertices[vert1_index+1]
+            vert2 = section_path_group.vertices[vert2_index]
             ori_verts.append(vert1)
             ori_verts.append(vert2)
             new_ver1 = this_face_normal*standoff_dist + vert1
             new_ver2 = this_face_normal * standoff_dist + vert2
 
-            translated_verts.append(new_ver1)
-            translated_verts.append(new_ver2)
+            translated_verts.append([x for x in vert1])
+            translated_verts.append([x for x in vert2])
+            translated_verts.append([x for x in new_ver1])
+            translated_verts.append([x for x in new_ver2])
 
-
+        # remove dublicates (happens when triangles are coplanar)
+        #translated_verts = list(set(set(tuple(i) for i in translated_verts)))
+        # new_entities = trimesh.path.entities.Line(np.array(translated_verts))
+        #new_section_entities.append(np.array(translated_verts))
+        translated_verts = np.transpose(np.array(translated_verts))
+        ax.plot(translated_verts[0], translated_verts[1], translated_verts[2])
+        new_section_entities = np.stack(np.array(translated_verts))
         print('\nori_verts', ori_verts, '\ntranslated_verts', translated_verts)
+print('new_section_entities', new_section_entities)
+new_section_entities = np.array(translated_verts)
+new_section_entities = np.transpose(new_section_entities)
+print('new_section_entities[:0]', new_section_entities[0])
 
+plt.show()
 combined3d = np.sum(d3sections)
+new_d3sections_combined = np.sum(new_d3sections+d3sections)
 #print(sections, sections[-1].vertices)
 #print(np.array(sections))
 combined = np.sum(sections)
 #print(combined, combined3d, combined3d.vertices)
 #combined.show()
-combined3d.show()
+#combined3d.show()
+new_d3sections_combined.show()
 def displace_segment_along_vector(path3D, vector, distance):
     return vector*distance+path3D
 
