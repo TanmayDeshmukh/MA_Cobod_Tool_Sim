4559f57a7e1380b1b7a13d72298d04658cc53816
Tanmay <tanmay.dn@gmail.com>
2021-02-07 21:46:55 +0100

    Sorting sub segments

--- main.py
+++ main.py
@@ -5,16 +5,14 @@ from __future__ import print_function
 import os
 import numpy as np
 from numpy import linalg as LA
-import stl
 import inspect
 import trimesh
 import itertools
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.patches import FancyArrowPatch
-from mpl_toolkits.mplot3d import proj3d
+
 import copy
+from viz_utils import *
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
@@ -23,20 +21,46 @@ from trimesh import voxel as v
 mesh = trimesh.load_mesh('models/wall_type_1.STL')
 
 fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
+ax.relim()
+# update ax.viewLim using the new dataLim
+ax.autoscale_view()
 min_lim = min(mesh.bounds[0,:])
 max_lim = max(mesh.bounds[1,:])
-ax.set_xlim3d(min_lim, max_lim)
-ax.set_ylim3d(min_lim,max_lim)
-ax.set_zlim3d(min_lim,max_lim)
+ax.set_xlim3d(mesh.bounds[0][0]-0.5, mesh.bounds[1][0]+0.5)
+ax.set_ylim3d(mesh.bounds[0][1],mesh.bounds[1][1])
+ax.set_zlim3d(mesh.bounds[0][2]-0.5,mesh.bounds[1][2]+0.5)
+
+limits = np.array([getattr(ax, f'get_{axis}lim')() for axis in 'xyz']);
+ax.set_box_aspect(np.ptp(limits, axis = 1))
+fig.tight_layout()
+fig.subplots_adjust(left=-0.1, right=1.0, top=1.2, bottom=-0.2)
+
 
 original_mesh = copy.copy(mesh)
 scene = trimesh.Scene()
 #scene.add_geometry(original_mesh)
+"""
+mplot = mplot3d.art3d.Poly3DCollection(original_mesh.triangles)
+mplot.set_alpha(0.5)
+mplot.set_facecolor('grey')
+#mplot.set_edgecolor('gray')
+mplot.set_sort_zpos(-2)
+ax.add_collection3d(mplot)
+"""
 faces_mask = np.array([i for i, normal in enumerate(mesh.face_normals) if normal[2]>0.5])
 print('faces_mask', faces_mask)
 mesh.update_faces(faces_mask)
 mesh.visual.face_colors = [50, 150, 50, 255]
 scene.add_geometry(mesh)
+
+"""
+mplot = mplot3d.art3d.Poly3DCollection(mesh.triangles)
+#mplot.set_alpha(0.6)
+mplot.set_facecolor('cornflowerblue')
+#mplot.set_edgecolor('k')
+mplot.set_sort_zpos(-1)
+ax.add_collection3d(mplot)
+"""
 
 y_extents = mesh.bounds[:,0]
 # slice every .10 model units
@@ -60,28 +84,6 @@ d3sectionsverts = [s.vertices for s in d3sections]
 face_normals = [mesh.face_normals[segment_face_indices] for segment_face_indices in face_indices]
 #print('face_normals', face_normals)
 
-class Arrow3D(FancyArrowPatch):
-    def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
-        self._verts3d = xs, ys, zs
-
-    def draw(self, renderer):
-        xs3d, ys3d, zs3d = self._verts3d
-        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
-        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
-        FancyArrowPatch.draw(self, renderer)
-
-def plot_path(ax, vertices: [[]]):
-
-
-    for i in range(1,len(vertices)):
-        a = Arrow3D([vertices[i-1][0], vertices[i][0]],
-                    [vertices[i-1][1], vertices[i][1]],
-                    [vertices[i-1][2], vertices[i][2]], mutation_scale=10,
-                    lw=1, arrowstyle="-|>", color="r")
-        ax.add_artist(a)
-    vertices = np.transpose(vertices)
-    #ax.plot(vertices[0], vertices[1], vertices[2])
 
 standoff_dist = 0.2
 vert_dist_threshold = 0.1
@@ -96,10 +98,12 @@ for section_iter, section_path_group in enumerate(d3sections):
     all_normals = []
     all_text_entities = []
     direction_flag = not direction_flag
+
     for subpath_iter, subpath in enumerate(section_path_group.entities):
         translated_verts = []
         ori_verts = []
         normals = []
+        ordered_subpath = copy.copy(subpath)
         #print('subpath.points', subpath.points, 'section_path_group attribs:\n', type(section_path_group), section_path_group.__dict__)
         for line_segment_index in range(len(subpath.points)-1):
             this_face_normal = mesh.face_normals[face_indices[line_segment_index]]
@@ -122,6 +126,7 @@ for section_iter, section_path_group in enumerate(d3sections):
         # check first 2 y values
         if (translated_verts[0][1] > translated_verts[1][1]) ^ direction_flag:
             translated_verts.reverse()
+            ordered_subpath.points=np.flip(ordered_subpath.points, axis=0)
             #all_text_entities.append(text_entity)
         #translated_verts = list(set([tuple(x) for x in translated_verts]))
 
@@ -131,18 +136,16 @@ for section_iter, section_path_group in enumerate(d3sections):
         filtered_trnaslated_verts =[translated_verts[0]]
         filtered_all_text_entities = []
         num = section_iter + len(all_verts) + len(filtered_trnaslated_verts)
-        vert_iter += 1
-        print('\nvert no.', num, section_iter, len(all_verts) , len(filtered_trnaslated_verts), vert_iter)
-        ax.text(filtered_trnaslated_verts[-1][0], filtered_trnaslated_verts[-1][1], filtered_trnaslated_verts[-1][2],
-                str(vert_iter), color='red')
+        #ax.text(filtered_trnaslated_verts[-1][0], filtered_trnaslated_verts[-1][1], filtered_trnaslated_verts[-1][2],
+        #        str(vert_iter), color='red', zorder = 2)
         for i in range(1, len(translated_verts)):
+            # if sample points are close enough and directions are the same, merge them
             if LA.norm(np.array(translated_verts[i])-np.array(translated_verts[i-1])) > vert_dist_threshold and tuple(normals[i-1])!= tuple(normals[i]) or i==len(translated_verts)-1:
-                vert_iter += 1
+
                 filtered_trnaslated_verts.append(translated_verts[i])
                 filtered_normals.append(normals[i])
                 num = section_iter + len(all_verts) + len(filtered_trnaslated_verts)
-                print('vert no.', num, section_iter, len(all_verts) , len(filtered_trnaslated_verts), vert_iter)
-                ax.text(filtered_trnaslated_verts[-1][0], filtered_trnaslated_verts[-1][1], filtered_trnaslated_verts[-1][2], str(vert_iter), color='red')
+
 
                 # text_entity = trimesh.path.entities.Text(origin=0, text='point '+str(len(filtered_trnaslated_verts)), height=0.5, vector=1, normal=0, align=None, layer=None)
                 # scene.add_geometry(text_entity)
@@ -150,33 +153,70 @@ for section_iter, section_path_group in enumerate(d3sections):
                 #text_entity.points = np.array(list(range(len(filtered_trnaslated_verts))))
                 #print('text_entity', type(text_entity),text_entity.__dict__, text_entity.points)
                 #filtered_all_text_entities.append(text_entity)
+
         entity = trimesh.path.entities.Line(points=list(range(len(all_verts), len(filtered_trnaslated_verts) + len(all_verts))))
-        new_entities.append(entity)
+
         all_text_entities += filtered_all_text_entities
         normals = np.array(normals)
         filtered_normals = np.array(filtered_normals)
-        all_verts += filtered_trnaslated_verts
+        #if all_verts:
+            # plot traversal without spray
+            #plot_path(ax, vertices=[all_verts[-1], filtered_trnaslated_verts[0]], color='k')
+            # insert into appropriate place
+        new_entities.append(entity)
+        """for vert_group_index in range(1,len(all_verts)):
+            if (filtered_trnaslated_verts[0][1] >= all_verts[vert_group_index-1][1] and filtered_trnaslated_verts[-1][1]<all_verts[vert_group_index][1] and direction_flag) or \
+                    (filtered_trnaslated_verts[0][1] <= all_verts[vert_group_index - 1][1] and
+                     filtered_trnaslated_verts[-1][1] > all_verts[vert_group_index]) :
+                all_verts.insert(vert_group_index, filtered_trnaslated_verts)
+                break
+                """
+        all_verts.append(filtered_trnaslated_verts)
         filtered_trnaslated_verts = np.array(filtered_trnaslated_verts)
         translated_verts = np.array(translated_verts)
-        vec = np.column_stack((filtered_trnaslated_verts, filtered_trnaslated_verts + (filtered_normals * .08)))
+        norm_vec = np.column_stack((filtered_trnaslated_verts, filtered_trnaslated_verts + (filtered_normals * .08)))
         #print('translated_verts', translated_verts, '\nfiltered_trnaslated_verts\n',filtered_trnaslated_verts,np.transpose(filtered_trnaslated_verts)[0])
-        normal_path = trimesh.load_path(vec.reshape((-1, 2, 3))) # returns Path3D object
-        normal_path.colors = [trimesh.visual.color.to_rgba([255, 0, 0, 255])]*len(vec)
+        normal_path = trimesh.load_path(norm_vec.reshape((-1, 2, 3))) # returns Path3D object
+        normal_path.colors = [trimesh.visual.color.to_rgba([255, 0, 0, 255])]*len(norm_vec)
         #print('normal path', normal_path)
         scene.add_geometry(normal_path)
-        plot_path(ax, vertices= filtered_trnaslated_verts)
+        #plot_path(ax, vertices= filtered_trnaslated_verts)
+        plot_normals(ax, vertices= filtered_trnaslated_verts, directions= -filtered_normals)
         #ax.plot(np.transpose(filtered_trnaslated_verts)[0], np.transpose(filtered_trnaslated_verts)[1], np.transpose(filtered_trnaslated_verts)[2])
 
+    ordered_all_verts = []
+    for i in range(len(all_verts)):
+        for j in range(i):
+            if ((not direction_flag) and all_verts[j][0][1] > all_verts[j+1][-1][1]):
+                print('flipping', all_verts[j], all_verts[j+1])
+                all_verts[j], all_verts[j+1] = all_verts[j+1], all_verts[j]
+                print('flipped', all_verts[j], all_verts[j + 1])
+    #vert_iter = 0
+    for ver_group in all_verts:
+        plot_path(ax, vertices= ver_group)
+        for vertex in ver_group:
+            ax.text(vertex[0], vertex[1], vertex[2],
+                    str(vert_iter), color='g', zorder=2)
+            if vert_iter == 26:
+                print('direction_flag', direction_flag)
+            vert_iter += 1
+    all_verts = list(itertools.chain.from_iterable(all_verts))
     all_verts=np.array(all_verts)
     #print('all verts', all_verts, 'new_entities', len(new_entities))
     #print('new_entities+filtered_all_text_entities', new_entities+all_text_entities)
-    path3ds = trimesh.path.path.Path3D(entities=new_entities, vertices=np.array(all_verts), metadata={}, colors=[trimesh.visual.color.to_rgba([0, 255, 0, 255])]*len(new_entities))
+    path3ds = trimesh.path.path.Path3D(entities=new_entities, vertices=all_verts, metadata={}, colors=[trimesh.visual.color.to_rgba([0, 255, 0, 255])]*len(new_entities))
     scene.add_geometry(path3ds)
     #print('\nori_verts', len(ori_verts), ori_verts, '\ntranslated_verts',len(translated_verts), translated_verts)
 
 #path3ds = trimesh.path.path.Path3D(entities=new_entities, vertices = np., metadata = section_path_group.metadata)
 
 #scene.show()
+
+# plot the ground plane
+xx, yy = np.meshgrid(np.arange(original_mesh.bounds[0][0], original_mesh.bounds[1][0], 0.25), np.arange(original_mesh.bounds[0][1], original_mesh.bounds[1][1], 0.25))
+z = np.full((len(xx), len(xx[0])), 0)
+ax.plot_surface(xx, yy, z, alpha=0.5)
+
 plt.show()
 combined3d = np.sum(d3sections)
 #print(sections, sections[-1].vertices)
---
+++ viz_utils.py
@@ -0,0 +1,46 @@
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+import numpy as np
+
+
+# Drawing arrows: https://stackoverflow.com/a/22867877
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer,)
+
+
+def plot_path(ax, vertices: [[]], color="g"):
+
+    for i in range(1,len(vertices)):
+        a = Arrow3D([vertices[i-1][0], vertices[i][0]],
+                    [vertices[i-1][1], vertices[i][1]],
+                    [vertices[i-1][2], vertices[i][2]], mutation_scale=10,
+                    lw=1, arrowstyle="-|>", color=color)
+        a.set_zorder(5)
+        ax.add_artist(a)
+    vertices = np.transpose(vertices)
+    #ax.plot(vertices[0], vertices[1], vertices[2])
+
+def plot_normals(ax, vertices: [[]], directions:[[]]):
+    vertices = np.array(vertices)
+    directions = np.array(directions)
+    norm_length = 0.15
+    for i in range(len(vertices)):
+        end = vertices[i] + norm_length*directions[i]
+        a = Arrow3D([vertices[i][0], end[0]],
+                    [vertices[i][1], end[1]],
+                    [vertices[i][2], end[2]], mutation_scale=10,
+                    lw=1, arrowstyle="-|>", color="r")
+        a.set_zorder(5)
+        ax.add_artist(a)
+    vertices = np.transpose(vertices)
+    #ax.plot(vertices[0], vertices[1], vertices[2])
